56d06292b6f1f54ec16b7b2547cc7f348f448c2d
Doryan <doryan.perez@salud.gob.sv>
2023-07-13 09:04:27 -0600

    commit inicial

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+/*root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+);*/
+
+root.render(<h1>i have been working for 4 years as </h1>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
